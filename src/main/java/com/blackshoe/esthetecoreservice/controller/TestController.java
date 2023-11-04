4b79ef60294f13cb3922d4c2f4ed0f1fd5f538af
Subin <70842920+bin-pro@users.noreply.github.com>
2023-11-04 17:13:14 +0900

    Chore: Add requestmapping

--- src/main/java/com/blackshoe/esthetecoreservice/controller/TestController.java
+++ src/main/java/com/blackshoe/esthetecoreservice/controller/TestController.java
@@ -1,9 +1,11 @@
 package com.blackshoe.esthetecoreservice.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("core")
 public class TestController {
     @GetMapping("/test")
     String test() {
