63710256cceaafcf5b548bf8529809f0e266eeb7
Hankyul Min <denirokr22@gmail.com>
2023-11-06 12:20:42 +0900

    Test: Exhibition, Room relation mapping, orphan removal

--- src/test/java/com/blackshoe/esthetecoreservice/repository/ExhibitionRepositoryTest.java
+++ src/test/java/com/blackshoe/esthetecoreservice/repository/ExhibitionRepositoryTest.java
@@ -1,6 +1,7 @@
 package com.blackshoe.esthetecoreservice.repository;
 
 import com.blackshoe.esthetecoreservice.entity.Exhibition;
+import com.blackshoe.esthetecoreservice.entity.Room;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
@@ -17,6 +18,15 @@ public class ExhibitionRepositoryTest {
     @Autowired
     private ExhibitionRepository exhibitionRepository;
 
+    @Autowired
+    private RoomRepository roomRepository;
+
+    private final Exhibition exhibition = Exhibition.builder()
+            .title("title")
+            .description("description")
+            .thumbnail("thumbnail")
+            .build();
+
     @Test
     public void assert_isNotNull() {
         assertThat(exhibitionRepository).isNotNull();
@@ -25,11 +35,6 @@ public class ExhibitionRepositoryTest {
     @Test
     public void save_returns_savedExhibition() {
         // given
-        final Exhibition exhibition = Exhibition.builder()
-                .title("title")
-                .description("description")
-                .thumbnail("thumbnail")
-                .build();
 
         // when
         final Exhibition savedExhibition = exhibitionRepository.save(exhibition);
@@ -42,5 +47,50 @@ public class ExhibitionRepositoryTest {
         assertThat(savedExhibition.getTitle()).isEqualTo(exhibition.getTitle());
         assertThat(savedExhibition.getDescription()).isEqualTo(exhibition.getDescription());
         assertThat(savedExhibition.getThumbnail()).isEqualTo(exhibition.getThumbnail());
+    }
+
+    @Test
+    public void addRoom_returns_addedRoom() {
+        // given
+        final Exhibition savedExhibition = exhibitionRepository.save(exhibition);
+
+        final Room room = Room.builder()
+                .title("title")
+                .description("description")
+                .thumbnail("thumbnail")
+                .exhibition(savedExhibition)
+                .build();
+
+        final Room savedRoom = roomRepository.save(room);
+
+        // when
+        savedExhibition.addRoom(savedRoom);
+
+        // then
+        assertThat(savedExhibition.getRooms().get(0)).isEqualTo(savedRoom);
+    }
+
+    @Test
+    public void deleteExhibition_onSuccess_alsoDeleteRoom() {
+        // given
+        final Exhibition savedExhibition = exhibitionRepository.save(exhibition);
+
+        final Room room = Room.builder()
+                .title("title")
+                .description("description")
+                .thumbnail("thumbnail")
+                .exhibition(savedExhibition)
+                .build();
+
+        final Room savedRoom = roomRepository.save(room);
+
+        savedExhibition.addRoom(savedRoom);
+
+        // when
+        exhibitionRepository.delete(savedExhibition);
+
+        // then
+        assertThat(exhibitionRepository.findById(savedExhibition.getId())).isEmpty();
+        assertThat(roomRepository.findById(savedRoom.getId())).isEmpty();
     }
 }
