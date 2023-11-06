6f2521ff3c66ace50a7659420dada34d5e7eda3b
Hankyul Min <denirokr22@gmail.com>
2023-11-06 11:40:03 +0900

    Test: PhotoRepository, PhotoUrlRepository bean creation

---
+++ src/test/java/com/blackshoe/esthetecoreservice/repository/PhotoUrlRepositoryTest.java
@@ -0,0 +1,19 @@
+package com.blackshoe.esthetecoreservice.repository;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@DataJpaTest
+public class PhotoUrlRepositoryTest {
+
+    @Autowired
+    private PhotoUrlRepository photoUrlRepository;
+
+    @Test
+    public void assert_isNotNull() {
+        assertThat(photoUrlRepository).isNotNull();
+    }
+}
