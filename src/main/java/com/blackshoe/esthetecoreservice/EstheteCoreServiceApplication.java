b00fee0f747b408a54c79e7311b8db7b5fb62e11
Hankyul Min <denirokr22@gmail.com>
2023-11-06 11:38:05 +0900

    Feat: Add @EnableJpaAuditing on Application class

--- src/main/java/com/blackshoe/esthetecoreservice/EstheteCoreServiceApplication.java
+++ src/main/java/com/blackshoe/esthetecoreservice/EstheteCoreServiceApplication.java
@@ -2,8 +2,10 @@ package com.blackshoe.esthetecoreservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class EstheteCoreServiceApplication {
 
     public static void main(String[] args) {
