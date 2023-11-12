e0252407ed12462888dd03c83e5ef4fb869681af
Hankyul Min <denirokr22@gmail.com>
2023-11-12 19:09:59 +0900

    Test: EBE-23 UserController getBasicInfo()

---
+++ src/test/java/com/blackshoe/esthetecoreservice/controller/UserControllerTest.java
@@ -0,0 +1,85 @@
+package com.blackshoe.esthetecoreservice.controller;
+
+import com.blackshoe.esthetecoreservice.dto.UserDto;
+import com.blackshoe.esthetecoreservice.exception.UserErrorResult;
+import com.blackshoe.esthetecoreservice.exception.UserException;
+import com.blackshoe.esthetecoreservice.service.UserService;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.data.jpa.mapping.JpaMetamodelMappingContext;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.mock.web.MockHttpServletResponse;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+
+import java.util.UUID;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@WebMvcTest(controllers = UserController.class)
+@MockBean(JpaMetamodelMappingContext.class)
+@AutoConfigureMockMvc
+public class UserControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockBean
+    private UserService userService;
+
+    private final ObjectMapper objectMapper = new ObjectMapper();
+
+    private final Logger log = LoggerFactory.getLogger(ExhibitionControllerTest.class);
+
+    @Test
+    public void getBasicInfo_whenSuccess_returns200() throws Exception {
+        // given
+        final UserDto.GetBasicInfoResponse userGetBasicInfoResponse = UserDto.GetBasicInfoResponse.builder()
+                .userId("userId")
+                .nickname("nickname")
+                .profileImg("profileImg")
+                .build();
+        when(userService.getBasicInfo(any(UUID.class))).thenReturn(userGetBasicInfoResponse);
+
+        // when
+        final MvcResult mvcResult = mockMvc.perform(
+                get("/users/{userId}/basic-info", UUID.randomUUID())
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andReturn();
+
+        // then
+        final MockHttpServletResponse response = mvcResult.getResponse();
+        assertThat(response.getStatus()).isEqualTo(HttpStatus.OK.value());
+        assertThat(response.getContentAsString()).isEqualTo(objectMapper.writeValueAsString(userGetBasicInfoResponse));
+    }
+
+    @Test
+    public void getBasicInfo_whenUserNotFound_returns404() throws Exception {
+        // given
+        when(userService.getBasicInfo(any(UUID.class))).thenThrow(new UserException(UserErrorResult.USER_NOT_FOUND));
+
+        // when
+        final MvcResult mvcResult = mockMvc.perform(
+                get("/users/{userId}/basic-info", UUID.randomUUID())
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isNotFound())
+                .andReturn();
+
+        // then
+        final MockHttpServletResponse response = mvcResult.getResponse();
+        assertThat(response.getStatus()).isEqualTo(HttpStatus.NOT_FOUND.value());
+        log.info("response: {}", response.getContentAsString());
+    }
+}
