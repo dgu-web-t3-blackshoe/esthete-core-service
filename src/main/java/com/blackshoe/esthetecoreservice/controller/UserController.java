b3152000a3e53394346aee53e6f9cf68f60ba1bc
Hankyul Min <denirokr22@gmail.com>
2023-11-19 21:43:04 +0900

    Feat: EBE-54 UserController searchByNicknameOrGenre()

--- src/main/java/com/blackshoe/esthetecoreservice/controller/UserController.java
+++ src/main/java/com/blackshoe/esthetecoreservice/controller/UserController.java
@@ -4,15 +4,23 @@ import com.blackshoe.esthetecoreservice.dto.ExhibitionDto;
 import com.blackshoe.esthetecoreservice.dto.GuestBookDto;
 import com.blackshoe.esthetecoreservice.dto.SupportDto;
 import com.blackshoe.esthetecoreservice.dto.UserDto;
+import com.blackshoe.esthetecoreservice.entity.User;
 import com.blackshoe.esthetecoreservice.service.GuestBookService;
 import com.blackshoe.esthetecoreservice.service.SupportService;
 import com.blackshoe.esthetecoreservice.service.UserService;
+import com.blackshoe.esthetecoreservice.vo.SortType;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
+import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 
 @RestController
@@ -64,5 +72,30 @@ public class UserController {
         SupportDto.DeleteResponse supportDeleteResponse = supportService.deleteSupport(userId, photographerId);
 
         return ResponseEntity.status(HttpStatus.OK).body(supportDeleteResponse);
+    }
+
+    @GetMapping("/search")
+    public ResponseEntity<Page<UserDto.SearchResult>> searchByNicknameOrGenre(@RequestParam(required = false) Optional<String> nickname,
+                                                                 @RequestParam(required = false) Optional<List<UUID>> genres,
+                                                                 @RequestParam(required = false, defaultValue = "recent") String sort,
+                                                                 @RequestParam(required = false, defaultValue = "0") Integer page,
+                                                                 @RequestParam(required = false, defaultValue = "10") Integer size) {
+
+        final Sort sortBy = SortType.convertParamToColumn(sort);
+        final Pageable pageable = PageRequest.of(page, size, sortBy);
+
+        Page<UserDto.SearchResult> readAllNicknameContainingPage = Page.empty();
+
+        if (nickname.isPresent() && genres.isEmpty()) {
+            readAllNicknameContainingPage = userService.readAllNicknameContaining(nickname.get(), pageable);
+        }
+        if (nickname.isEmpty() && genres.isPresent()) {
+            readAllNicknameContainingPage = userService.readAllGenresContaining(genres.get(), pageable);
+        }
+        if (nickname.isPresent() && genres.isPresent()) {
+            readAllNicknameContainingPage = userService.readAllNicknameAndGenreContaining(nickname.get(), genres.get(), pageable);
+        }
+
+        return ResponseEntity.status(HttpStatus.OK).body(readAllNicknameContainingPage);
     }
 }
---
+++ src/main/java/com/blackshoe/esthetecoreservice/vo/SortType.java
@@ -0,0 +1,30 @@
+package com.blackshoe.esthetecoreservice.vo;
+
+import com.blackshoe.esthetecoreservice.exception.UserErrorResult;
+import com.blackshoe.esthetecoreservice.exception.UserException;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Sort;
+
+@Getter
+@RequiredArgsConstructor
+public enum SortType {
+    POPULAR("supportCount"),
+    TRENDING("viewCount"),
+    RECENT("updatedAt");
+
+    private final String sortType;
+
+    public static Sort convertParamToColumn(String sort) {
+        switch (sort) {
+            case "popular":
+                return Sort.by(Sort.Direction.DESC, POPULAR.sortType);
+            case "trending":
+                return Sort.by(Sort.Direction.DESC, TRENDING.sortType);
+            case "recent":
+                return Sort.by(Sort.Direction.DESC, RECENT.sortType);
+            default:
+                throw new UserException(UserErrorResult.INVALID_SORT_TYPE);
+        }
+    }
+}
