ba9d61ad3c9f930cf4142628e07507458c8f87df
Hankyul Min <denirokr22@gmail.com>
2023-11-26 01:50:39 +0900

    Feat: EBE-34 PhotoController readByAddress

--- src/main/java/com/blackshoe/esthetecoreservice/controller/PhotoController.java
+++ src/main/java/com/blackshoe/esthetecoreservice/controller/PhotoController.java
@@ -3,10 +3,14 @@ package com.blackshoe.esthetecoreservice.controller;
 import com.blackshoe.esthetecoreservice.dto.PhotoDto;
 import com.blackshoe.esthetecoreservice.service.PhotoService;
 import com.blackshoe.esthetecoreservice.vo.LocationGroupType;
+import com.blackshoe.esthetecoreservice.vo.PhotoAddressFilter;
 import com.blackshoe.esthetecoreservice.vo.PhotoPointFilter;
+import com.blackshoe.esthetecoreservice.vo.PhotoSortType;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
@@ -14,18 +18,21 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 //objectMapper
+import java.util.Optional;
 import java.util.UUID;
 
 @RequiredArgsConstructor
-@RequestMapping("/core/photos") @Slf4j @RestController
+@RequestMapping("/core/photos")
+@Slf4j
+@RestController
 public class PhotoController {
 
     private final PhotoService photoService;
 
     @PostMapping(consumes = {MediaType.MULTIPART_FORM_DATA_VALUE})
     public ResponseEntity<PhotoDto.UploadResponse> uploadPhoto(@RequestPart(name = "photo") MultipartFile photo,
-                                                  @RequestPart(name = "photo_upload_request")
-                                                  PhotoDto.CreateRequest photoUploadRequest) {
+                                                               @RequestPart(name = "photo_upload_request")
+                                                               PhotoDto.CreateRequest photoUploadRequest) {
         //log all
         log.info("photoUploadRequest: {}", photoUploadRequest);
 
@@ -57,9 +64,9 @@ public class PhotoController {
 
     @GetMapping("/locations/current")
     public ResponseEntity<Page<PhotoDto.ReadRegionGroupResponse>> getTop10ByUserLocationGroupBy(@RequestParam(name = "longitude") double longitude,
-                                                                                         @RequestParam(name = "latitude") double latitude,
-                                                                                          @RequestParam(name = "radius") double radius,
-                                                                                          @RequestParam(name = "group") String group) {
+                                                                                                @RequestParam(name = "latitude") double latitude,
+                                                                                                @RequestParam(name = "radius") double radius,
+                                                                                                @RequestParam(name = "group") String group) {
         final PhotoPointFilter photoPointFilter = PhotoPointFilter.builder()
                 .longitude(longitude)
                 .latitude(latitude)
@@ -72,6 +79,29 @@ public class PhotoController {
                 = photoService.getTop10ByUserLocationGroupBy(photoPointFilter, locationGroupType);
 
         return ResponseEntity.status(HttpStatus.OK).body(photoReadRegionGroupResponse);
+    }
+
+    @GetMapping("/locations")
+    public ResponseEntity<Page<PhotoDto.ReadResponse>> readByAddress (
+            @RequestParam(name = "state", required = true) Optional<String> state,
+            @RequestParam(name = "city", required = false) Optional<String> city,
+            @RequestParam(name = "town", required = false) Optional<String> town,
+            @RequestParam(name = "page") Integer page,
+            @RequestParam(name = "size") Integer size,
+            @RequestParam(name = "sort") String sort) {
+
+        final PhotoAddressFilter photoAddressFilter = PhotoAddressFilter.builder()
+                .state(state.orElse(""))
+                .city(city.orElse(""))
+                .town(town.orElse(""))
+                .build();
+
+        final Sort sortBy = PhotoSortType.convertParamToColumn(sort);
+
+        final Page<PhotoDto.ReadResponse> photoReadByAddressResponse
+                = photoService.readByAddress(photoAddressFilter, page, size, sortBy);
+
+        return ResponseEntity.status(HttpStatus.OK).body(photoReadByAddressResponse);
     }
 
 }
