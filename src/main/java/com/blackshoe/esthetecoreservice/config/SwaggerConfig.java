ad21fdc946b2638f0fcde5895cb0efc034fc056d
SUBIN <70842920+bin-pro@users.noreply.github.com>
2023-11-26 11:31:15 +0900

    Chore: Update SwaggerConfig.java

--- src/main/java/com/blackshoe/esthetecoreservice/config/SwaggerConfig.java
+++ src/main/java/com/blackshoe/esthetecoreservice/config/SwaggerConfig.java
@@ -1,22 +1,14 @@
 package com.blackshoe.esthetecoreservice.config;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
-import org.springframework.http.HttpHeaders;
-
-//import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
-import springfox.documentation.service.*;
+import springfox.documentation.service.ApiInfo;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
-
 
 @Configuration
 public class SwaggerConfig {
@@ -30,7 +22,7 @@ public class SwaggerConfig {
                 //.ignoredParameterTypes(AuthenticationPrincipal.class)
                 .apiInfo(apiInfo())
                 .select()
-                .apis(RequestHandlerSelectors.basePackage("com.blackshoe.esthetecoreservice.controller"))
+                .apis(RequestHandlerSelectors.any())
                 .paths(PathSelectors.any())
                 .build();
     }
