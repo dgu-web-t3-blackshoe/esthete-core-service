bd5e24f70da3e1d09ecac4d706fa3f3382d6a0b0
Subin <70842920+bin-pro@users.noreply.github.com>
2023-11-27 04:40:53 +0900

    Chore: Add Servers for swagger

--- src/main/java/com/blackshoe/esthetecoreservice/config/SwaggerConfig.java
+++ src/main/java/com/blackshoe/esthetecoreservice/config/SwaggerConfig.java
@@ -1,5 +1,7 @@
 package com.blackshoe.esthetecoreservice.config;
 
+import springfox.documentation.service.Server;
+import springfox.documentation.spi.DocumentationType;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
@@ -9,6 +11,7 @@ import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.service.ApiInfo;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
+import java.util.Collections;
 
 @Configuration
 public class SwaggerConfig {
@@ -18,8 +21,11 @@ public class SwaggerConfig {
     @Profile({"test || dev"})
     @Bean
     public Docket api() {
+        Server serverLocal = new Server("local", "http://localhost:8080", "for local usages", Collections.emptyList(), Collections.emptyList());
+        Server testingServer = new Server("production", "http://34.22.84.195", "for testing", Collections.emptyList(), Collections.emptyList());
+        Server productionServer = new Server("production", "https://api.esthete.roberniro-projects.xyz", "for production", Collections.emptyList(), Collections.emptyList());
         return new Docket(DocumentationType.OAS_30)
-                //.ignoredParameterTypes(AuthenticationPrincipal.class)
+                .servers(serverLocal, testingServer, productionServer)
                 .apiInfo(apiInfo())
                 .select()
                 .apis(RequestHandlerSelectors.any())
